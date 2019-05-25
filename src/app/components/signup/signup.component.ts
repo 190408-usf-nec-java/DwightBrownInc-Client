1a74ed9fd08b04d0a6fa65cac845c968b1deca7c
sarmand19 <sarmand19@gmail.com>
2019-05-25 18:23:38 -0400

    added click functionality to submmit button

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SignupService } from 'src/app/services/signup/signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  firstname = '';
+  lastname = '';
+  email = '';
+  signupResponse: Subscription;
+  lastStatus = 201;
 
-  constructor() { }
+  constructor(private signUpService: SignupService ) { }
 
   ngOnInit() {
+    this.signupResponse = this.signUpService.$signUpStatus.subscribe(status =>
+     if (status === 201){
+       alert(' Congratulations Signup Succesfull');
+     } else{
+       alert('Sorry Signup is Unsuccessful');
+       this.lastStatus = status;
+     } );
   }
 
 }
